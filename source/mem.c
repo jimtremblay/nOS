c2d5f3b3c4a76c3b52a4204c9ddd7ea48b8b5e4b
Jim Tremblay <jimtremblay@gmail.com>
2014-07-29 15:01:18 -0400

    fix mem wait to allow polling from isr/threads/main/sched locked

--- source/mem.c
+++ source/mem.c
@@ -13,6 +13,29 @@
 extern "C" {
 #endif
 
+/*
+ * Name        : nOS_MemCreate
+ *
+ * Description : Create a dynamic array of fixed block size.
+ *
+ * Arguments   : mem    : Pointer to mem object.
+ *               buffer : Pointer to array of memory.
+ *               bsize  : Size of ONE block of memory.
+ *               max    : Maximum number of blocks your buffer can hold.
+ *
+ * Return      : Error code.
+ *               NOS_E_NULL    : Pointer to mem object is NULL.
+ *                               OR
+ *                               Pointer to buffer array is NULL.
+ *               NOS_E_INV_VAL : Size of ONE block is too small.
+ *                               OR
+ *                               Maximum number of blocks is 0.
+ *               NOS_OK        : Mem created with success.
+ *
+ * Note        : Mem object must be created before using it, else
+ *               behaviour is undefined and must be called one time
+ *               ONLY for each mem object.
+ */
 nOS_Error nOS_MemCreate (nOS_Mem *mem, void *buffer, size_t bsize, uint16_t max)
 {
     nOS_Error   err;
@@ -33,6 +56,7 @@ nOS_Error nOS_MemCreate (nOS_Mem *mem, void *buffer, size_t bsize, uint16_t max)
     {
         nOS_CriticalEnter();
         nOS_EventCreate((nOS_Event*)mem);
+        /* Initialize the single-link list */
         list = NULL;
         for (i = 0; i < max-1; i++) {
             *(void**)buffer = list;
@@ -50,6 +74,27 @@ nOS_Error nOS_MemCreate (nOS_Mem *mem, void *buffer, size_t bsize, uint16_t max)
     return err;
 }
 
+/*
+ * Name        : nOS_MemAlloc
+ *
+ * Description : Wait on mem object for ONE block of memory. If no block available,
+ *               calling thread will be placed in object's waiting list for number
+ *               of ticks specified by tout. If a block of memory is freed before
+ *               end of timeout, thread will be awoken and pointer to memory block
+ *               will be returned.
+ *
+ * Arguments   : mem   : Pointer to mem object.
+ *               tout  : Timeout value
+ *                       NOS_NO_WAIT      : No waiting.
+ *                       NOS_WAIT_INIFINE : Never timeout.
+ *                       0 > tout < 65535 : Number of ticks to wait on mem object.
+ *
+ * Return      : Pointer to allocated block of memory.
+ *               == NULL : No block available.
+ *               != NULL : Pointer to newly allocated block of memory.
+ *
+ * Note        : Caller is responsible to free the block when memory is no longer needed.
+ */
 void *nOS_MemAlloc(nOS_Mem *mem, uint16_t tout)
 {
     int8_t  err;
@@ -66,14 +111,21 @@ void *nOS_MemAlloc(nOS_Mem *mem, uint16_t tout)
             block = (void*)mem->list;
             mem->list = *(void**)block;
             mem->count--;
-        } else if (tout > 0) {
+        /* Caller can't wait? Try again. */
+        } else if (tout == NOS_NO_WAIT) {
+            block = NULL;
+        } else if (nOS_isrNestingCounter > 0) {
+            block = NULL;
+        } else if (nOS_lockNestingCounter > 0) {
+            block = NULL;
+        } else if (nOS_runningThread == &nOS_mainThread) {
+            block = NULL;
+        } else {
             nOS_runningThread->context = (void*)&block;
             err = nOS_EventWait((nOS_Event*)mem, NOS_MEM_ALLOC, tout);
             if (err != NOS_OK) {
                 block = NULL;
             }
-        } else {
-            block = NULL;
         }
         nOS_CriticalLeave();
     }
