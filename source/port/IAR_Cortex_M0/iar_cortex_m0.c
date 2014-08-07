26a86421cdc23ebb2f5a0b01e8ae585018483e51
Jim Tremblay <jimtremblay@gmail.com>
2014-08-06 23:09:43 -0400

    add iar cortex-m0 port

---
+++ source/port/IAR_Cortex_M0/iar_cortex_m0.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2014 Jim Tremblay
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#define NOS_PRIVATE
+#include "nOS.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+NOS_STACK(isrStack[NOS_CONFIG_ISR_STACK_SIZE]);
+
+void nOS_PortInit(void)
+{
+    nOS_CriticalEnter();
+    /* Copy msp to psp */
+    __set_PSP(__get_MSP());
+    /* Set msp to local isr stack */
+    __set_MSP(((unsigned long)&isrStack[NOS_CONFIG_ISR_STACK_SIZE-1]) & 0xfffffff8UL);
+    /* Set current stack to psp and priviledge mode */
+    __set_CONTROL(__get_CONTROL() | 0x00000002UL);
+    /* Set PendSV and SysTick to lowest priority */
+    *(volatile uint32_t *)0xe000ed20UL |= 0xffff0000UL;
+    nOS_CriticalLeave();
+}
+
+void nOS_ContextInit(nOS_Thread *thread, stack_t *stack, size_t ssize, void(*func)(void*), void *arg)
+{
+    stack_t *tos = (stack_t*)((stack_t)(stack + (ssize - 1)) & 0xfffffff8UL);
+
+    *(--tos) = 0x01000000UL;    /* xPSR */
+    *(--tos) = (stack_t)func;   /* PC */
+    *(--tos) = 0x00000000UL;    /* LR */
+    tos     -= 4;               /* R12, R3, R2 and R1 */
+    *(--tos) = (stack_t)arg;    /* R0 */
+    tos     -= 8;               /* R11, R10, R9, R8, R7, R6, R5 and R4 */
+
+    thread->stackPtr = tos;
+}
+
+void nOS_IsrEnter (void)
+{
+    nOS_CriticalEnter();
+    nOS_isrNestingCounter++;
+    nOS_CriticalLeave();
+}
+
+void nOS_IsrLeave (void)
+{
+    nOS_CriticalEnter();
+    nOS_isrNestingCounter--;
+    if (nOS_isrNestingCounter == 0) {
+        if (nOS_lockNestingCounter == 0) {
+            nOS_highPrioThread = SchedHighPrio();
+            if (nOS_runningThread != nOS_highPrioThread) {
+                *(volatile uint32_t *)0xe000ed04UL = 0x10000000UL;
+            }
+        }
+    }
+    nOS_CriticalLeave();
+}
+
+#if defined(__cplusplus)
+}
+#endif
---
+++ source/port/IAR_Cortex_M0/iar_cortex_m0_asm.s
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2014 Jim Tremblay
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+    RSEG    CODE:CODE(2)
+    thumb
+
+    EXTERN nOS_runningThread
+    EXTERN nOS_highPrioThread
+
+    PUBLIC PendSV_Handler
+
+PendSV_Handler:
+    /* Disable interrupt */
+    CPSID       I
+    ISB
+
+    /* Save PSP before doing anything, PendSV_Handler already running on MSP */
+    MRS         R0,        PSP
+    ISB
+
+    /* Get the location of nOS_runningThread */
+    LDR         R3,         =nOS_runningThread
+    LDR         R2,         [R3]
+
+    /* Make space for the remaining registers */
+    SUBS        R0,         R0,                 #32
+
+    /* Save PSP to nOS_Thread object of current running thread */
+    STR         R0,         [R2]
+
+    /* Push low registers on thread stack */
+    STMIA       R0!,        {R4-R7}
+    
+    /* Copy high registers to low registers */
+    MOV         R4,         R8
+    MOV         R5,         R9
+    MOV         R6,         R10
+    MOV         R7,         R11
+    /* Push high registers on thread stack */
+    STMIA       R0!,        {R4-R7}
+
+    /* Copy nOS_highPrioThread to nOS_runningThread */
+    LDR         R1,         =nOS_highPrioThread
+    LDR         R0,         [R1]
+    STR         R0,         [R3]
+
+    /* Restore psp from nOS_Thread object of high prio thread */
+    LDR         R2,         [R1]
+    LDR         R0,         [R2]
+
+    /* Move to the high registers */
+    ADDS        R0,         R0,                 #16
+    
+    /* Pop high registers from thread stack */
+    LDMIA       R0!,        {R4-R7}
+    /* Copy low registers to high registers */
+    MOV         R11,        R7
+    MOV         R10,        R6
+    MOV         R9,         R5
+    MOV         R8,         R4
+
+    /* Restore PSP to high prio thread stack */
+    MSR         PSP,        R0
+    ISB
+
+    /* Go back for the low registers */
+    SUBS        R0,         R0,                 #32
+
+    /* Pop low registers from thread stack */
+    LDMIA       R0!,        {R4-R7}
+
+    /* Enable interrupt */
+    CPSIE       I
+    ISB
+
+    BX          LR
+    NOP
+
+    /* Not needed in this file */
+    /* END */
