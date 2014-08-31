43f0c5e3dd3158b3d87ea06c5c64727f6341a78c
Jim Tremblay <jimtremblay@gmail.com>
2014-08-31 12:19:02 -0400

    remove test of alignment

--- examples/emBlocks-GCC_STM32L152C-Discovery/src/main.c
+++ examples/emBlocks-GCC_STM32L152C-Discovery/src/main.c
@@ -69,9 +69,9 @@ int main(void)
     nOS_SemCreate(&semB, 0, 1);
     nOS_SemCreate(&semC, 0, 1);
 
-    nOS_ThreadCreate(&threadA, ThreadA, 0, &stackA[1], THREAD_STACK_SIZE-1, NOS_CONFIG_HIGHEST_THREAD_PRIO,   NOS_READY);
-    nOS_ThreadCreate(&threadB, ThreadB, 0, &stackB[1], THREAD_STACK_SIZE-1, NOS_CONFIG_HIGHEST_THREAD_PRIO-1, NOS_READY);
-    nOS_ThreadCreate(&threadC, ThreadC, 0, &stackC[1], THREAD_STACK_SIZE-1, NOS_CONFIG_HIGHEST_THREAD_PRIO-2, NOS_READY);
+    nOS_ThreadCreate(&threadA, ThreadA, 0, stackA, THREAD_STACK_SIZE, NOS_CONFIG_HIGHEST_THREAD_PRIO,   NOS_READY);
+    nOS_ThreadCreate(&threadB, ThreadB, 0, stackB, THREAD_STACK_SIZE, NOS_CONFIG_HIGHEST_THREAD_PRIO-1, NOS_READY);
+    nOS_ThreadCreate(&threadC, ThreadC, 0, stackC, THREAD_STACK_SIZE, NOS_CONFIG_HIGHEST_THREAD_PRIO-2, NOS_READY);
 
     while (1) {
         nOS_SemGive(&semC);
