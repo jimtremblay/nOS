6aa7db3d32600bc9be9dfd8005144ced1a844fd4
Jim Tremblay <jimtremblay@gmail.com>
2014-08-31 11:57:12 -0400

    basic example for ARM Cortex M3 using EmBlocks with GCC

---
+++ examples/emBlocks-GCC_STM32L1-Discovery/src/main.c
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014 Jim Tremblay
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "stm32l1xx_conf.h"
+#include "nOS.h"
+
+#define TICKS_PER_SECOND            1000
+
+#define THREAD_STACK_SIZE           64
+
+nOS_Sem semA;
+nOS_Sem semB;
+nOS_Sem semC;
+nOS_Thread threadA;
+nOS_Thread threadB;
+nOS_Thread threadC;
+nOS_Stack stackA[THREAD_STACK_SIZE];
+nOS_Stack stackB[THREAD_STACK_SIZE];
+nOS_Stack stackC[THREAD_STACK_SIZE];
+
+NOS_ISR(SysTick_Handler)
+{
+    nOS_Tick();
+}
+
+void ThreadA (void *arg)
+{
+    NOS_UNUSED(arg);
+
+    while (1) {
+        nOS_SemTake(&semA, NOS_WAIT_INFINITE);
+    }
+}
+
+void ThreadB (void *arg)
+{
+    NOS_UNUSED(arg);
+
+    while (1) {
+        nOS_SemTake(&semB, NOS_WAIT_INFINITE);
+        nOS_SemGive(&semA);
+    }
+}
+
+void ThreadC (void *arg)
+{
+    NOS_UNUSED(arg);
+
+    while (1) {
+        nOS_SemTake(&semC, NOS_WAIT_INFINITE);
+        nOS_SemGive(&semB);
+    }
+}
+
+int main(void)
+{
+    // No interrupt should occurs before nOS_Init
+    nOS_Init();
+
+    SystemCoreClockUpdate();
+    SysTick_Config(SystemCoreClock / TICKS_PER_SECOND);
+
+    nOS_SemCreate(&semA, 0, 1);
+    nOS_SemCreate(&semB, 0, 1);
+    nOS_SemCreate(&semC, 0, 1);
+
+    nOS_ThreadCreate(&threadA, ThreadA, 0, &stackA[1], THREAD_STACK_SIZE-1, NOS_CONFIG_HIGHEST_THREAD_PRIO,   NOS_READY);
+    nOS_ThreadCreate(&threadB, ThreadB, 0, &stackB[1], THREAD_STACK_SIZE-1, NOS_CONFIG_HIGHEST_THREAD_PRIO-1, NOS_READY);
+    nOS_ThreadCreate(&threadC, ThreadC, 0, &stackC[1], THREAD_STACK_SIZE-1, NOS_CONFIG_HIGHEST_THREAD_PRIO-2, NOS_READY);
+
+    while (1) {
+        nOS_SemGive(&semC);
+    }
+}
