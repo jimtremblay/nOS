003210f1882a867d69b72bc282a25b4eec5384d7
Jim Tremblay <jimtremblay@gmail.com>
2014-09-07 13:58:02 -0400

    add keil arm cortex m0+ example

---
+++ examples/Keil_FRDM-KL25Z/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Keil_FRDM-KL25Z' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_KLxx    /* Device Startup for KLxx */
+
+#endif /* RTE_COMPONENTS_H */
