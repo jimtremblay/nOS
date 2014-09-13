961e4358b5691ce4a6f115c5746c5cf363745a6b
Jim Tremblay <jimtremblay@gmail.com>
2014-09-12 22:46:26 -0400

    add iar msp430 port

---
+++ source/port/IAR/MSP430/port.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2014 Jim Tremblay
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#define NOS_PRIVATE
+#include "nOS.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(NOS_CONFIG_ISR_STACK_SIZE)
+ #if (NOS_CONFIG_ISR_STACK_SIZE == 0)
+  #error "nOSConfig.h: NOS_CONFIG_ISR_STACK_SIZE is set to invalid value."
+ #else
+  static nOS_Stack isrStack[NOS_CONFIG_ISR_STACK_SIZE];
+ #endif
+#endif
+
+void nOS_ContextInit(nOS_Thread *thread, nOS_Stack *stack, size_t ssize, void(*func)(void*), void *arg)
+{
+    /* Stack grow from high to low address */
+    nOS_Stack   *tos    = stack + (ssize - 1);
+    uint16_t    *tos16;
+    
+#if (NOS_CONFIG_DEBUG > 0)
+    /* Place some fences around thread's stack to know if an overflow occurred */
+    *stack++ = (nOS_Stack)0x01234567;
+    *stack   = (nOS_Stack)0x89abcdef;
+    *tos--   = (nOS_Stack)0x76543210;
+    *tos--   = (nOS_Stack)0xfedcba98;
+#endif
+       
+     tos16   = (uint16_t*)tos;
+#if (__CORE__ == __430X__)
+    *tos16-- = (uint16_t)((uint32_t)func >> 16);    /* PC MSB */
+#endif
+    *tos16-- = (uint16_t)((uint32_t)func);          /* PC LSB */
+    *tos16-- = 0x0008;                              /* SR */
+#if (__CORE__ == __430X__) && ((__DATA_MODEL__ == __DATA_MODEL_MEDIUM__) || (__DATA_MODEL__ == __DATA_MODEL_LARGE__))
+     tos16  -= 1;
+#endif
+     tos     = (nOS_Stack*)tos16;
+     
+#if (NOS_CONFIG_DEBUG > 0)
+    *tos-- = (nOS_Stack)0x15151515;     /* R15 */
+    *tos-- = (nOS_Stack)0x14141414;     /* R14 */
+    *tos-- = (nOS_Stack)0x13131313;     /* R13 */
+#else
+     tos  -= 3;                         /* R15 to R13 */
+#endif
+    *tos-- = (nOS_Stack)arg;            /* R12 */
+#if (NOS_CONFIG_DEBUG > 0)
+    *tos-- = (nOS_Stack)0x11111111;     /* R11 */
+    *tos-- = (nOS_Stack)0x10101010;     /* R10 */
+    *tos-- = (nOS_Stack)0x09090909;     /* R9 */
+    *tos-- = (nOS_Stack)0x08080808;     /* R8 */
+    *tos-- = (nOS_Stack)0x07070707;     /* R7 */
+    *tos-- = (nOS_Stack)0x06060606;     /* R6 */
+    *tos-- = (nOS_Stack)0x05050505;     /* R5 */
+    *tos   = (nOS_Stack)0x04040404;     /* R4 */
+#else
+     tos  -= 7;                         /* R11 to R4 */
+#endif
+
+    thread->stackPtr = tos;
+}
+
+__task void nOS_ContextSwitch(void)
+{
+    __asm (
+        /* Simulate an interrupt by pushing SR */
+         PUSH_SR
+        "                                           \n"
+        /* Save all registers to running thread stack */
+         PUSH_CONTEXT
+        "                                           \n"
+        /* Save stack pointer to running thread structure */
+         MOV_X"     &nOS_runningThread,     R12     \n"
+         MOV_X"     SP,                     0(R12)  \n"
+        "                                           \n"
+        /* Copy nOS_highPrioThread to nOS_runningThread */
+         MOV_X"     &nOS_highPrioThread,    R12     \n"
+         MOV_X"     #nOS_runningThread,     R11     \n"
+         MOV_X"     R12,                    0(R11)  \n"
+        "                                           \n"
+        /* Restore stack pointer from high prio thread structure */
+         MOV_X"     @R12,                   SP      \n"
+        "                                           \n"
+        /* Pop all registers from high prio thread stack */
+         POP_CONTEXT
+        "                                           \n"
+         POP_SR
+         RET_X
+    );
+}
+
+nOS_Stack* nOS_IsrEnter (nOS_Stack *sp)
+{
+    nOS_CriticalEnter();
+    if (nOS_isrNestingCounter == 0) {
+        nOS_runningThread->stackPtr = sp;
+#if defined(NOS_CONFIG_ISR_STACK_SIZE)
+        sp = &isrStack[NOS_CONFIG_ISR_STACK_SIZE-1];
+#else
+        sp = nOS_mainThread.stackPtr;
+#endif
+    }
+    nOS_isrNestingCounter++;
+    nOS_CriticalLeave();
+    
+    return sp;
+}
+
+nOS_Stack* nOS_IsrLeave (nOS_Stack *sp)
+{
+    nOS_CriticalEnter();
+    nOS_isrNestingCounter--;
+    if (nOS_isrNestingCounter == 0) {
+#if (NOS_CONFIG_SCHED_LOCK_ENABLE > 0)
+        if (nOS_lockNestingCounter == 0)
+#endif
+        {
+            nOS_highPrioThread = SchedHighPrio();
+            nOS_runningThread = nOS_highPrioThread;
+        }
+        sp = nOS_runningThread->stackPtr;
+    }
+    nOS_CriticalLeave();
+    
+    return sp;
+}
+
+#if defined(__cplusplus)
+}
+#endif
---
+++ source/port/IAR/MSP430/port.h
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2014 Jim Tremblay
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef PORT_H
+#define PORT_H
+
+#include <stdint.h>
+#include <intrinsics.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if (__CORE__ == __430X__)
+#if (__DATA_MODEL__ == __DATA_MODEL_SMALL__)
+typedef uint16_t                nOS_Stack;
+#else
+typedef uint32_t                nOS_Stack;
+#endif
+#else
+typedef uint16_t                nOS_Stack;
+#endif
+
+#define NOS_UNUSED(v)           (void)v
+
+#define NOS_MEM_ALIGNMENT       2
+
+#if (__DATA_MODEL__ == __DATA_MODEL_SMALL__)
+#define     PUSH_X              "PUSH.W"
+#define     POP_X               "POP.W"
+#define     PUSHM_X             "PUSHM.W"
+#define     POPM_X              "POPM.W"
+#define     MOV_X               "MOV.W"
+#endif
+
+#if (__DATA_MODEL__ == __DATA_MODEL_MEDIUM__)
+#define     PUSH_X              "PUSHX.A"
+#define     POP_X               "POPX.A"
+#define     PUSHM_X             "PUSHM.A"
+#define     POPM_X              "POPM.A"
+#define     MOV_X               "MOV.W"
+#endif
+
+#if (__DATA_MODEL__ == __DATA_MODEL_LARGE__)
+#define     PUSH_X              "PUSHX.A"
+#define     POP_X               "POPX.A"
+#define     PUSHM_X             "PUSHM.A"
+#define     POPM_X              "POPM.A"
+#define     MOV_X               "MOVX.A"
+#endif
+
+#if (__CORE__ == __430X__)
+#if (__CODE_MODEL__ == __CODE_MODEL_SMALL__)
+#define     CALL_X              "CALL"
+#define     RET_X               "RET"
+#endif
+#if (__CODE_MODEL__ == __CODE_MODEL_LARGE__)
+#define     CALL_X              "CALLA"
+#define     RET_X               "RETA"
+#endif
+#else
+#define     CALL_X              "CALL"
+#define     RET_X               "RET"
+#endif
+
+#define PUSH_SR                 "PUSH.W     SR              \n"
+#define POP_SR                  "POP.W      SR              \n"
+#if (__CORE__ == __430X__)
+#define     PUSH_CONTEXT        PUSHM_X"    #12,    R15     \n"
+#define     POP_CONTEXT         POPM_X"     #12,    R15     \n"
+#else
+#define     PUSH_CONTEXT                                                        \
+    "PUSH   R15             \n"                                                 \
+    "PUSH   R14             \n"                                                 \
+    "PUSH   R13             \n"                                                 \
+    "PUSH   R12             \n"                                                 \
+    "PUSH   R11             \n"                                                 \
+    "PUSH   R10             \n"                                                 \
+    "PUSH   R9              \n"                                                 \
+    "PUSH   R8              \n"                                                 \
+    "PUSH   R7              \n"                                                 \
+    "PUSH   R6              \n"                                                 \
+    "PUSH   R5              \n"                                                 \
+    "PUSH   R4              \n"
+#define     POP_CONTEXT                                                         \
+    "POP    R4              \n"                                                 \
+    "POP    R5              \n"                                                 \
+    "POP    R6              \n"                                                 \
+    "POP    R7              \n"                                                 \
+    "POP    R8              \n"                                                 \
+    "POP    R9              \n"                                                 \
+    "POP    R10             \n"                                                 \
+    "POP    R11             \n"                                                 \
+    "POP    R12             \n"                                                 \
+    "POP    R13             \n"                                                 \
+    "POP    R14             \n"                                                 \
+    "POP    R15             \n"
+#endif
+
+#define nOS_CriticalEnter()                                                     \
+{                                                                               \
+    __istate_t _sr = __get_interrupt_state();                                   \
+    __disable_interrupt()
+
+#define nOS_CriticalLeave()                                                     \
+    __set_interrupt_state(_sr);                                                 \
+}
+
+#define NOS_ISR(vect)                                                           \
+__task void vect##_ISR_L2(void);                                                \
+void vect##_ISR_L3(void);                                                       \
+_Pragma(_STRINGIFY(vector=##vect))                                              \
+__interrupt __raw void vect##_ISR(void)                                         \
+{                                                                               \
+    vect##_ISR_L2();                                                            \
+}                                                                               \
+_Pragma("required=nOS_IsrEnter")                                                \
+_Pragma("required=nOS_IsrLeave")                                                \
+__task void vect##_ISR_L2(void)                                                 \
+{                                                                               \
+    __asm (                                                                     \
+        PUSH_SR                                                                 \
+        PUSH_CONTEXT                                                            \
+        MOV_X"  SP,     R12                     \n"                             \
+        CALL_X" #nOS_IsrEnter                   \n"                             \
+        MOV_X"  R12,    SP                      \n"                             \
+        CALL_X" #"_STRINGIFY(vect##_ISR_L3)"    \n"                             \
+        MOV_X"  SP,     R12                     \n"                             \
+        CALL_X" #nOS_IsrLeave                   \n"                             \
+        MOV_X"  R12,    SP                      \n"                             \
+        POP_CONTEXT                                                             \
+        POP_SR                                                                  \
+        RET_X                                                                   \
+    );                                                                          \
+}                                                                               \
+void vect##_ISR_L3(void)
+
+/* Unused function for this port */
+#define nOS_PortInit()
+
+void        nOS_ContextInit     (nOS_Thread *thread, nOS_Stack *stack, size_t ssize, void(*func)(void*), void *arg);
+__task void nOS_ContextSwitch   (void);
+
+nOS_Stack*  nOS_IsrEnter        (nOS_Stack *sp);
+nOS_Stack*  nOS_IsrLeave        (nOS_Stack *sp);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* PORT_H */
