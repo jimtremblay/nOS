9a4a37f93d24adf98aa22c576530dbfed1ea15ed
Jim Tremblay <jimtremblay@gmail.com>
2015-01-17 08:44:32 -0500

    add MPLAB XC16 PIC24 example (can be used on board DB-DP11112)

---
+++ examples/MPLABX-XC16_PIC24FJ256GB106.X/main.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include "nOS.h"
+
+// Using the 20MHz clock provided by the PIC24FJ256GB106
+_CONFIG2(IESO_OFF & PLL_96MHZ_ON & PLLDIV_DIV5 & FNOSC_PRIPLL & POSCMOD_HS)   // Primary HS OSC with PLL, USBPLL /3
+_CONFIG1(JTAGEN_OFF & ICS_PGx2 & FWDTEN_OFF)        // JTAG off, watchdog timer off
+
+#define THREAD_STACK_SIZE       128
+
+void ThreadA(void *arg);
+void ThreadB(void *arg);
+void ThreadC(void *arg);
+
+nOS_Sem semA;
+nOS_Sem semB;
+nOS_Sem semC;
+nOS_Thread threadA;
+nOS_Thread threadB;
+nOS_Thread threadC;
+nOS_Stack threadAStack[THREAD_STACK_SIZE];
+nOS_Stack threadBStack[THREAD_STACK_SIZE];
+nOS_Stack threadCStack[THREAD_STACK_SIZE];
+
+void ThreadA(void *arg)
+{
+    volatile uint32_t cntr = 0;
+
+    (void)arg;
+
+    while (1) {
+        nOS_SemTake(&semA, NOS_WAIT_INFINITE);
+        cntr++;
+    }
+}
+
+void ThreadB(void *arg)
+{
+    volatile uint32_t cntr = 0;
+    cntr |= 0x80;
+
+    (void)arg;
+
+    while (1) {
+        nOS_SemTake(&semB, NOS_WAIT_INFINITE);
+        nOS_SemGive(&semA);
+        cntr++;
+    }
+}
+
+void ThreadC(void *arg)
+{
+    volatile uint32_t cntr = 0;
+
+    (void)arg;
+
+    while (1) {
+        nOS_SemTake(&semC, NOS_WAIT_INFINITE);
+        nOS_SemGive(&semB);
+        cntr++;
+    }
+}
+
+void TimerInit(void)
+{
+    T2CON = 0;
+    TMR2 = 0;
+
+    PR2 = ( uint16_t ) 50000;
+
+    IPC1bits.T2IP = NOS_CONFIG_MAX_UNSAFE_ISR_PRIO;
+
+    IFS0bits.T2IF = 0;
+
+    IEC0bits.T2IE = 1;
+
+    T2CONbits.TON = 1;
+}
+
+NOS_ISR(_T2Interrupt)
+{
+    /* Clear the timer interrupt. */
+    IFS0bits.T2IF = 0;
+
+    nOS_Tick();
+}
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    volatile uint32_t cntr = 0;
+
+    nOS_Init();
+
+    TimerInit();
+
+    nOS_SemCreate(&semA, 0, 1);
+    nOS_SemCreate(&semB, 0, 1);
+    nOS_SemCreate(&semC, 0, 1);
+
+    nOS_ThreadCreate(&threadA, ThreadA, (void*)300, threadAStack, THREAD_STACK_SIZE, 5, NOS_THREAD_READY);
+    nOS_ThreadCreate(&threadB, ThreadB, (void*)200, threadBStack, THREAD_STACK_SIZE, 4, NOS_THREAD_READY);
+    nOS_ThreadCreate(&threadC, ThreadC, (void*)100, threadCStack, THREAD_STACK_SIZE, 3, NOS_THREAD_READY);
+
+    while (1) {
+        nOS_SemGive(&semC);
+        cntr++;
+    }
+}
+
---
+++ examples/MPLABX-XC16_PIC24FJ256GB106.X/nOSConfig.h
@@ -0,0 +1,95 @@
+/* Define to value other than 0 to enable debug code. */
+#define NOS_CONFIG_DEBUG                        1
+
+/* Define to value other than 0 to enable arguments checking in nOS API. */
+#define NOS_CONFIG_SAFE                         1
+
+/* Highest priority a thread can take (up to 255). */
+#define NOS_CONFIG_HIGHEST_THREAD_PRIO          15
+
+/* Size of ticks counter for sleep/timeout in bits (can be 8, 16, 32 and 64). */
+#define NOS_CONFIG_TICK_COUNT_WIDTH             16
+
+/* Enable or disable scheduler locking. */
+#define NOS_CONFIG_SCHED_LOCK_ENABLE            1
+/* Enable or disable sleeping from running thread. */
+#define NOS_CONFIG_SLEEP_ENABLE                 1
+/* Enable or disable sleeping until specified from running thread. */
+#define NOS_CONFIG_SLEEP_UNTIL_ENABLE           1
+
+/* Enable or disable thread resume/suspend at run-time. */
+#define NOS_CONFIG_THREAD_SUSPEND_ENABLE        1
+/* Enable or disable thread deletion at run-time. */
+#define NOS_CONFIG_THREAD_DELETE_ENABLE         1
+/* Enable or disable thread set/get priority at run-time. */
+#define NOS_CONFIG_THREAD_SET_PRIO_ENABLE       1
+
+/* Enable or disable semaphore. */
+#define NOS_CONFIG_SEM_ENABLE                   1
+/* Enable or disable semaphore deletion at run-time. */
+#define NOS_CONFIG_SEM_DELETE_ENABLE            1
+/* Semaphore count width in bits (can be 8, 16 or 32) */
+#define NOS_CONFIG_SEM_COUNT_WIDTH              16
+
+/* Enable or disable mutex. */
+#define NOS_CONFIG_MUTEX_ENABLE                 1
+/* Enable or disable mutex deletion at run-time. */
+#define NOS_CONFIG_MUTEX_DELETE_ENABLE          1
+/* Enable or disable recursive mutex */
+#define NOS_CONFIG_MUTEX_RECURSIVE_ENABLE       1
+
+/* Enable or disable flag. */
+#define NOS_CONFIG_FLAG_ENABLE                  1
+/* Enable or disable flag deletion at run-time. */
+#define NOS_CONFIG_FLAG_DELETE_ENABLE           1
+/* Size of flag in bits (can be 8, 16, 32 or 64) */
+#define NOS_CONFIG_FLAG_NB_BITS                 16
+
+/* Enable or disable queue. */
+#define NOS_CONFIG_QUEUE_ENABLE                 1
+/* Enable or disable queue deletion at run-time. */
+#define NOS_CONFIG_QUEUE_DELETE_ENABLE          1
+
+/* Enable or disable mem. */
+#define NOS_CONFIG_MEM_ENABLE                   1
+/* Enable or disable mem deletion at run-time. */
+#define NOS_CONFIG_MEM_DELETE_ENABLE            1
+/* Enable or disable sanity check of pointer when it is freed. */
+#define NOS_CONFIG_MEM_SANITY_CHECK_ENABLE      1
+
+/* Enable or disable timer thread with callback. */
+#define NOS_CONFIG_TIMER_ENABLE                 1
+/* Enable or disable dynamic timer deletion */
+#define NOS_CONFIG_TIMER_DELETE_ENABLE          1
+/* Priority of timer thread. */
+#define NOS_CONFIG_TIMER_THREAD_PRIO            1
+/* Stack size of timer thread. */
+#define NOS_CONFIG_TIMER_THREAD_STACK_SIZE      128
+/* Timer counter width in bits (can be 8, 16 or 32) */
+#define NOS_CONFIG_TIMER_COUNT_WIDTH            16
+
+/* Enable or disable time management. */
+#define NOS_CONFIG_TIME_ENABLE                  1
+/* Enable or disable time waiting from running thread. */
+#define NOS_CONFIG_TIME_WAIT_ENABLE             1
+/* Time ticks per second */
+#define NOS_CONFIG_TIME_TICKS_PER_SECOND        1000
+/* Time ticks counter width in bits (can be 32 or 64). */
+#define NOS_CONFIG_TIME_COUNT_WIDTH             32
+
+/*
+ * Stack size to use from interrupt routines in number of nOS_Stack entries.
+ * Not used on all platforms.
+ */
+#define NOS_CONFIG_ISR_STACK_SIZE               128
+
+/*
+ * Highest priority of interrupt routines that use nOS API which can enable
+ * zero interrupt latency for high priority ISR. You should not call any nOS
+ * API from interrupt handlers with priority higher than this setting. Can't
+ * be set to zero.
+ * Lower number = Higher priority (except PIC24)
+ * Higher number = Higher priority (PIC24 only, max value: 7)
+ * Not used on all platforms.
+ */
+#define NOS_CONFIG_MAX_UNSAFE_ISR_PRIO          4
